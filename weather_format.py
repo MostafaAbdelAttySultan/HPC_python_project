8c9d232a352a0f986152e8a9081fb60ca34204ad
Mostafa AbdelAtty Sultan <mostafaismae2002@gmail.com>
2024-07-31 13:19:08 +0300

    After formatting

--- weather_format.py
+++ weather_format.py
@@ -1,11 +1,12 @@
 import re
 from prettytable import PrettyTable
+
 with open('wearher_json.json', 'r') as f:
     data = f.read()
-temp =re.search(r'"temp":\s*(\d+\.\d+)', data).group(1)
-feels_like =re.search(r'"feels_like":\s*(\d+\.*\d+)', data).group(1)
-pressure =re.search(r'"pressure":\s*(\d+\.*\d+)', data).group(1)
-hum =re.search(r'"humidity":\s*(\d+\.*\d+)', data).group(1)
+temp = re.search(r'"temp":\s*(\d+\.\d+)', data).group(1)
+feels_like = re.search(r'"feels_like":\s*(\d+\.*\d+)', data).group(1)
+pressure = re.search(r'"pressure":\s*(\d+\.*\d+)', data).group(1)
+hum = re.search(r'"humidity":\s*(\d+\.*\d+)', data).group(1)
 t = PrettyTable(['City', 'Alexandria'])
 t.add_row(['Temperature K', temp])
 t.add_row(['Feels like C', (float(feels_like) - 273)])
@@ -13,5 +14,3 @@ t.add_row(['Pressure', pressure])
 t.add_row(['Humidity', hum])
 with open('weather_data.txt', 'w') as f:
     f.write(t.get_string())
-
-
